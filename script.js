128dc4111d2c13756eba9c19aaa9f665656690a2
Remy <harropjackmax1@gmail.com>
2025-07-13 00:16:49 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
  window.addEventListener('load', async function () {
   let playerCount = 0;
-  let playerRecord = parseInt(localStorage.getItem("playerRecord")) || 21334520;
+  let playerRecord = parseInt(localStorage.getItem("playerRecord")) || 0;
   
   const sheet = document.styleSheets[0]; // First stylesheet
   const experienceId = "7436755782";
